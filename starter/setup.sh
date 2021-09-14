4e362e3cf01614f9df02ab81f8e44eef0dd763b3
Abdulmohsen2 <yourgitemail@example.com>
2021-09-14 20:45:48 +0300

    V1

---
+++ starter/setup.sh
@@ -0,0 +1,5 @@
+export AUTH0_DOMAIN="dev-muwzm-jp.us.auth0.com"
+export API_AUDIENCE="Casting"
+export ALGORITHMS=['RS256']
+export FLASK_ENV=development
+export DATABASE_URL="postgres://cbihdexishwbdl:0f66c565f9263f0ac6ebcba8e15d17b09b2d1995a287681262e5a6a35f5d6f3c@ec2-54-156-24-159.compute-1.amazonaws.com:5432/d4ne2ou5nsod00"
